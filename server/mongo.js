b70d3195fbc5a5e4d84e81a60b085d0c6328ef6c
Emre KARDAŞ <eekardass@yahoo.com>
2025-01-02 11:48:41 +0000

    mongoDB URl

--- server/mongo.js
+++ server/mongo.js
@@ -1,6 +1,10 @@
-
+require('dotenv').config();
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://eekardas:2415emre@coffice.mk4ow.mongodb.net/?retryWrites=true&w=majority&appName=Coffice";
+
+const uri = process.env.MONGODB_URI;
+if (!uri) {
+  throw new Error('Please define MONGODB_URI in .env file');
+}
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -13,14 +17,16 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
+    // Connect the client to the server
     await client.connect();
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
+    console.log("Successfully connected to MongoDB!");
+    return client;
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+    throw error;
   }
 }
-run().catch(console.dir);
+
+module.exports = { run, client };
